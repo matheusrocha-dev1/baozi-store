2d979371d7ea91c76e64779c9922b846c05211f0
Matheus Rodrigues <m.rocha.dev1@gmail.com>
2026-05-10 03:03:10 -0300

    feat: configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/baozistore/baozistore/BaoziStoreApplication.java
@@ -0,0 +1,13 @@
+package com.baozistore.baozistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoziStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaoziStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baozistore/baozistore/BaoziStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baozistore.baozistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaoziStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
